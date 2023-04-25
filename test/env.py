ea9919973d200c5a198134ebef946f853b49c352
lorry <ruiqingluo1@163.com>
2023-04-25 16:10:04 +0800

    beachlanding env

---
+++ test/env.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from hmacl.envs.beachlanding.combat_env_vs_bot import CombatEnv
+
+parser = ArgumentParser()
+parser.add_argument("--scenario", type=str, default="5u_vs_5u")
+parser.add_argument("--camp", type=str, default="offensive")
+parser.add_argument("--reward_type", type=str, default="sparse")
+parser.add_argument("--time_limit", type=int, default=200)
+parser.add_argument("--map_size", type=list, default=[20, 20])
+parser.add_argument("--expand_degree", type=int, default=5)
+config = parser.parse_args()
+
+env = CombatEnv(**config.__dict__)
+env.reset()
+agents = env.agent_idx_dict
