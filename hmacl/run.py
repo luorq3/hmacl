8cd370752002b43f6f12256651141a4a95f6af72
lorry <ruiqingluo1@163.com>
2023-04-28 17:13:50 +0800

    hmacl config

--- hmacl/run.py
+++ hmacl/run.py
@@ -1,5 +1,8 @@
-import datetime
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+import datetime
 import random
 import numpy as np
 from types import SimpleNamespace as NameSpace
