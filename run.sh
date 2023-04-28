e91ad93a3af9c38904de3383d2ab245bc41a5140
lorry <ruiqingluo1@163.com>
2023-04-28 17:21:46 +0800

    hmacl config

--- run.sh
+++ run.sh
@@ -3,10 +3,11 @@
 name=vdn_ns
 env=m2ale
 use_wandb=True
+wandb_job_type=Training
 
 for seed in {1..3}
 do
   echo "Started with seed=${seed}."
-  python hmacl/run.py --name=$name --env=$env --seed="$seed" --use_wandb=$use_wandb
+  python hmacl/run.py --name=$name --env=$env --seed="$seed" --use_wandb=$use_wandb --wandb_job_type=$wandb_job_type
   echo "Done with seed=${seed}."
 done
