44b90e3c6adf0ac80fd484f25858fb6840b167c2
lorry <ruiqingluo1@163.com>
2023-05-07 20:09:35 +0800

    hmacl ray tune

---
+++ hmacl/train.py
@@ -0,0 +1,43 @@
+import datetime
+import multiprocessing
+
+from run import search
+
+
+_CPU_COUNT = multiprocessing.cpu_count() - 1
+
+cpus = _CPU_COUNT
+num_seeds = 5
+
+config = {
+    # required
+    "project": "exp-v5",
+    "name": "vdn_ns",
+    "env": "m2ale",
+    # experiment name and dir
+    "exp": datetime.datetime.now().strftime('%m-%d_%H-%M-%s'),
+    "exp_exist_ok": True,
+    # hmacl
+    "loss_coef": 0.1,
+    "temperature": 0.1,
+    # time
+    "t_max": 4000000,
+    "t_update": 20000,
+    # algo
+    "buffer_size": 5000,
+    "lr": 0.0005,
+    "hidden_dim": 128,
+    "epsilon_anneal_time": 100000,
+    "use_rnn": True,
+    # log and file path
+    "wandb_job_type": "Training",
+    "use_wandb": True
+}
+
+configs = []
+pool = multiprocessing.Pool(processes=cpus)
+for i in range(1, 5):
+    config["seed"] = i
+    configs.append(config)
+pool.map(search, configs)
+
--- run.sh
+++ run.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# required
+project="exp-v5"
 name=vdn_ns
 env=m2ale
-use_wandb=True
+# experiment name and dir
+exp=$(date +%Y-%m-%d_%T)
+exp_exist_ok=True
+# hmacl
+loss_coef=0.1
+temperature=0.1
+# time
+t_max=4000000
+t_update=20000
+# algo
+buffer_size=5000
+lr=0.0005
+hidden_dim=128
+epsilon_anneal_time=100000
 use_rnn=True
+# log and file path
 wandb_job_type=Training
+use_wandb=True
 
-for seed in {1..3}
+
+for seed in {1..5}
 do
   echo "Started with seed=${seed}."
-  # python hmacl/run.py --name=vdn_ns --env=m2ale --seed=3 --use_wandb=True --wandb_job_type=Training --use_rnn=True
-  python hmacl/run.py --name=$name --env=$env --seed="$seed" --use_wandb=$use_wandb --wandb_job_type=$wandb_job_type --use_rnn=$use_rnn
+  echo hmacl/run.py --project=$project --name=$name --env=$env --seed="$seed" --exp="$exp" --exp_exist_ok=$exp_exist_ok --loss_coef=$loss_coef --temperature=$temperature --t_max=$t_max --t_update=$t_update --buffer_size=$buffer_size --lr=$lr --hidden_dim=$hidden_dim --epsilon_anneal_time=$epsilon_anneal_time --use_rnn=$use_rnn --wandb_job_type=$wandb_job_type --use_wandb=$use_wandb
   echo "Done with seed=${seed}."
 done
