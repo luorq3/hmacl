815c479ae2830b995ec5408fc62b04a43be07678
lorry <ruiqingluo1@163.com>
2023-05-15 15:12:04 +0800

    hmacl vdn_cs config

--- hmacl/config.py
+++ hmacl/config.py
@@ -19,6 +19,7 @@ def get_config():
     parser.add_argument("--loss_coef", type=float, default=0.5, help="")
     parser.add_argument("--temperature", type=float, default=1, help="")
     parser.add_argument("--save_cpi_model", default=True, type=lambda x: bool(strtobool(x)), help="")
+    parser.add_argument("--cps", default=False, type=lambda x: bool(strtobool(x)), help="")
     # time
     parser.add_argument("--t_max", type=int, default=2040000, help="")
     parser.add_argument("--t_update", type=int, default=20000, help="")
