df1d6527126267362721e5ae3f654da64d8a8efe
lorry <ruiqingluo1@163.com>
2023-05-15 13:58:54 +0800

    hmacl training script

--- hmacl/algo/controllers/__init__.py
+++ hmacl/algo/controllers/__init__.py
@@ -3,7 +3,9 @@ REGISTRY = {}
 from .basic_controller import BasicMAC
 from .non_shared_controller import NonSharedMAC
 from .maddpg_controller import MADDPGMAC
+from .class_shared_controller import ClassSharedMAC
 
 REGISTRY["basic_mac"] = BasicMAC
 REGISTRY["non_shared_mac"] = NonSharedMAC
 REGISTRY["maddpg_mac"] = MADDPGMAC
+REGISTRY["class_shared_mac"] = ClassSharedMAC
